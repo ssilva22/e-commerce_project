b2359da24908f9020f29d9f391e2c8dbaed2ec75
Sebastian <sebastianwebp@gmail.com>
2022-02-05 15:06:02 -0500

    installed and added main project configuration to use Tailwindcss as well as other dependencies that will be used

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
 }
 
-module.exports = nextConfig
+console.log("next.config.js", JSON.stringify(module.exports, null, 2))
