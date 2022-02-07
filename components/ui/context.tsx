8bd81ac206b74eeda5e8c12aaf419e9d9779c626
Sebastian <sebastianwebp@gmail.com>
2022-02-07 08:47:19 -0500

    added reducer for UI provider

--- components/ui/context.tsx
+++ components/ui/context.tsx
@@ -1,4 +1,4 @@
-import { useContext,useState } from "react"
+import { useContext,useReducer } from "react"
 import { createContext } from "react"
 import {FC} from "react"
 
@@ -25,14 +25,36 @@ const UIContext = createContext<State>({
    ...initialState
 })
 
+type Action = { type: "OPEN_SIDEBAR"  | "CLOSE_SIDEBAR" }
+
+
+function uiReducer(state:StateValues,action:Action) {
+    switch(action.type) {
+        case "OPEN_SIDEBAR": {
+            return {
+                ...state,
+                isSidebarOpen:true
+            }
+        }
+        case "CLOSE_SIDEBAR": {
+            return {
+                ...state,
+                isSidebarOpen:false
+            }
+        }   
+     }
+}
+
 export const UIProvider:FC = ({children}) => {
-    const openSidebar = () => alert("Opening Sidebar")
-    const closeSidebar = () => alert("Closing Sidebar")
+    const [state,dispatch] = useReducer(uiReducer,initialState)
+
+    const openSidebar = () => dispatch({type:"OPEN_SIDEBAR"})
+    const closeSidebar = () => dispatch({type:"CLOSE_SIDEBAR"})
 
     const value ={
+        ...state,
         openSidebar,
         closeSidebar,
-        isSidebarOpen:false
     }
 
     return (
