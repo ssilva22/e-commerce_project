aedb3b5fdd832016f5eb35ccbb964100958e6321
Sebastian <sebastianwebp@gmail.com>
2022-02-08 12:58:44 -0500

    refactor:ProductSlider to have a keen-slider

--- components/product/ProductSlider/ProductSlider.tsx
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -1,13 +1,43 @@
 
-import { FC } from "react"
+import React, { FC, Children, isValidElement } from "react"
 import s from "./ProductSlider.module.css"
+import { useKeenSlider } from "keen-slider/react"
 
 const ProductSlider: FC = ({children}) => {
 
+  const [sliderRef, _] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(s) {
+      console.log("changing to slide: ", s.details().relativeSlide)
+    },
+  })
+
   return (
     <div className={s.root}>
-      <div className="h-full transition-opacity">
-        {children}
+      <div ref={sliderRef as any} className="keen-slider h-full transition-opacity">
+        { Children.map(children, child => {
+          if (isValidElement(child)) {
+            return {
+              ...child,
+              props: {
+                ...child.props,
+                className: `${
+                  child.props.className ? `${child.props.className}` : ""
+                } keen-slider__slide`
+              }
+            }
+
+            // return React.cloneElement(child, {
+            //   className: `${
+            //     child.props.className ? `${child.props.className}` : ""
+            //   } keen-slider__slide`
+            // })
+          }
+
+          return child
+        }
+        )}
       </div>
     </div>
   )
