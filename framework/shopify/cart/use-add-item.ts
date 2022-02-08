4902538b608510dc1e4e86bd3d8fdff42de1c2dc
Sebastian <sebastianwebp@gmail.com>
2022-02-08 18:55:03 -0500

    refactor: added hook to the common Api provider and simplified shopify apiProvider

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,18 +1,20 @@
 
 
-import { useAddItem } from "@common/cart";
+import { useAddItem } from "@common/cart"
 
 export default useAddItem
 
+
 export const handler = {
-    fetcher: () => {
-        console.log("Fetching Data!")
-    },
-    useHook: () => {
-        return (input: any) => {
-            return {
-                output: JSON.stringify(input) + "_MODIFIED"
-            }
-        }
+  fetcher: (input: any) => {
+    return JSON.stringify(input) + "_MODIFIED"
+  },
+  useHook: ({fetch}: any) => {
+    return (input: any) => {
+      const response = fetch(input)
+      return {
+        output: response
+      }
     }
+  }
 }
